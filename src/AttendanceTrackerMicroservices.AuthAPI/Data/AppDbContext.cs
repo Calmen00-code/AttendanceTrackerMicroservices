a1fe99915e6bcf29a62e16a56af07111acefb078
Calmen <calmenchia@gmail.com>
2025-10-14 22:43:01 +0800

    Setting up authentication API service
    - Added database for authentication
    - Configuring project structure

---
+++ src/AttendanceTrackerMicroservices.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AttendanceTrackerMicroservices.AuthAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
+
+
+
