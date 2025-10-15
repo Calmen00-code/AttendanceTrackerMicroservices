143a8ec0f312a8b45acd8514c4d867be7b3ea63f
Calmen <calmenchia@gmail.com>
2025-10-15 22:49:39 +0800

    Prepare API interface for Authentication Service
    - Service for Login and Register

---
+++ src/AttendanceTrackerMicroservices.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using AttendanceTrackerMicroservices.AuthAPI.Data;
+using AttendanceTrackerMicroservices.AuthAPI.Models;
+using AttendanceTrackerMicroservices.AuthAPI.Models.DTO;
+using AttendanceTrackerMicroservices.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace AttendanceTrackerMicroservices.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<UserDTO> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AttendanceTrackerMicroservices.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using AttendanceTrackerMicroservices.AuthAPI.Models.DTO;
+
+namespace AttendanceTrackerMicroservices.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDTO> Register(RegistrationRequestDTO registrationRequestDTO);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+    }
+}
