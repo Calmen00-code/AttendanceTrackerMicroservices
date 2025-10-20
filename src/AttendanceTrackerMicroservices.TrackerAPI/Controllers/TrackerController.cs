78cac4f71e1ff2a05673c331b4f8087b14e26426
Calmen <calmenchia@gmail.com>
2025-10-20 17:01:44 +0800

    Initial project for TrackerAPI services

---
+++ src/AttendanceTrackerMicroservices.TrackerAPI/Controllers/TrackerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttendanceTrackerMicroservices.TrackerAPI.Controllers
+{
+    [Route("api/tracker")]
+    [ApiController]
+    public class TrackerController : ControllerBase
+    {
+    }
+}
