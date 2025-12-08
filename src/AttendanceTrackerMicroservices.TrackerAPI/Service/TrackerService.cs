5e3c1bc701802a78105e8d8351b21121106d548f
Calmen <calmenchia@gmail.com>
2025-12-09 00:18:26 +0800

    Check Out option for user when scanned QR
    Web:
    - QRController now can call its own tracker service to perform Checkout
      functionalities
    - Added a new DTO for DailyAttendanceRecord
    
    TrackerAPI:
    - Introduce new API for checkout async
    - Introduce helper method to find the correct checkout record from
      database
    - Added a new DTO for DailyAttendanceRecordDTO to distinct it from
      DailyAttendanceRecord coming from database, as DTO does not require
      valid Id

--- src/AttendanceTrackerMicroservices.TrackerAPI/Service/TrackerService.cs
+++ src/AttendanceTrackerMicroservices.TrackerAPI/Service/TrackerService.cs
@@ -22,7 +22,7 @@ namespace AttendanceTrackerMicroservices.TrackerAPI.Service
                 .ToListAsync();
         }
 
-        public async Task<string> AddNewDailyAttendanceRecordAsync(DailyAttendanceRecord record)
+        public async Task<string> CheckInAsync(DailyAttendanceRecord record)
         {
             try
             {
@@ -34,6 +34,47 @@ namespace AttendanceTrackerMicroservices.TrackerAPI.Service
                 return ex.Message;
             }
             return string.Empty;
+        }
+
+        public async Task<string> CheckOutAsync(DailyAttendanceRecord record)
+        {
+            // To find the check-out record, we need to find:
+            //  1. UserID that help us to identify which user we are looking at
+            //  2. The date of the check-in matches to the record passed in as parameter
+            //     (this is because one user could have multiple record on the same day)
+            //  3. And finally, the check-out value must be default value (DateTime.MinValue)
+            DailyAttendanceRecord? checkOutRecord =
+                await _db.DailyAttendanceRecords.FirstOrDefaultAsync(u =>
+                    u.UserId == record.UserId &&
+                    u.CheckIn.Date == record.CheckIn.Date &&
+                    u.CheckOut == DateTime.MinValue);
+
+            if (checkOutRecord == null)
+            {
+                return "Checkout record cannot be found!";
+            }
+
+            _db.Update(checkOutRecord);
+            await _db.SaveChangesAsync();
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Helper method to find the correct checkout record from database
+        /// </summary>
+        /// <param name="request">Incoming checkout request from client</param>
+        /// <returns>Returns the correct record back to the client API, so that correct record is used</returns>
+        public async Task<DailyAttendanceRecord?> FindCheckOutRecordFromRequest(DailyAttendanceRecordDTO request)
+        {
+            // To find the correct checkout record:
+            // 1. UserId must match
+            // 2. There must be existing record in database with Check-In value
+            //    same date as the request's Check-Out
+            // 3. Since we do not checkout yet, then the existing checkout value must be DateTime.MinValue
+            return await _db.DailyAttendanceRecords.FirstOrDefaultAsync(u =>
+                u.UserId == request.UserId &&
+                u.CheckIn.Date == request.CheckOut.Date &&
+                u.CheckOut == DateTime.MinValue);
         }
     }
 }
